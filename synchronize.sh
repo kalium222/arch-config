4bd9ebb7f3d2f2e5764f7094f912b1ce7cd83fff
kalium <kalium222@outlook.com>
2024-06-04 22:02:35 +0800

    feat: leftwm theme; xinitrc; docker source

--- synchronize.sh
+++ synchronize.sh
@@ -30,7 +30,7 @@ sync_config() {
 sync_config "$source_dir" "$target_dir"
 
 # synchronize the other things
-files=(".bashrc" ".zshrc" ".shrc" ".condarc" ".Xmodmap")
+files=(".bashrc" ".zshrc" ".shrc" ".condarc" ".Xmodmap" ".xinitrc")
 for file in "${files[@]}"; do
     ln -sf "$(pwd)/$file" "$HOME/$file"
 done
